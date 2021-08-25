a85a300d7e5fabd2a2ecc528f67f95a3ae2a68c5
dave.w <email@daves.computer>
2021-08-25 11:11:55 -0400

    Release script to limit image resolution

---
+++ release_quality.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+print_quality () {
+    echo "Converting $1 to print quality"
+    BN=$(basename "$1")
+    gs -q \
+    -dNOPAUSE \
+    -dBATCH -dSAFER \
+    -sDEVICE=pdfwrite \
+    -dCompatabilityLevel=1.5 \
+    -dPDFSETTINGS=/printer \
+    -dEmbedAllFonts=true \
+    -dSubsetFonts=true \
+    -sOutputFile="output/print_quality/$BN" \
+    "$1"
+}
+
+ebook_quality () {
+    echo "Converting $1 to ebook quality"
+    BN=$(basename "$1")
+    gs -q \
+    -dNOPAUSE \
+    -dBATCH -dSAFER \
+    -sDEVICE=pdfwrite \
+    -dCompatabilityLevel=1.5 \
+    -dPDFSETTINGS=/ebook \
+    -dEmbedAllFonts=true \
+    -dSubsetFonts=true \
+    -sOutputFile="output/ebook_quality/$BN" \
+    "$1"
+}
+
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+for f in $(find output/build -iname "*.pdf"); do
+    print_quality "$f" &
+    ebook_quality "$f" &
+done
+wait
+IFS=$SAVEIFS
